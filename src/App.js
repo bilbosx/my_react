bbbd48b5451ba0598e356b362e34e465383e1ca7
bilbos <bilboobivan@gmail.com>
2022-06-21 19:47:28 +0300

    https

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'http://www.omdbapi.com?apikey=358358d0';
+const API_URL = 'https://www.omdbapi.com?apikey=358358d0';
 
 const movie1 = {
   "Title": "Amazing Spiderman Syndrome",
